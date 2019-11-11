27aa4edc5dce5a653a520a489f1aa81f5c1c951d
Sentinel <wildpython@yandex.ru>
2019-11-11 06:39:27 +0300

    файл со списками объектов, с которыми сталкивается персонаж

---
+++ intersectionObjects.py
@@ -0,0 +1,9 @@
+import GameObject
+from GameObject import *
+
+#кровать, шкаф, стол, подоконник, вешалка
+bedroom = [GameObject(17, 422, 368, 168),
+GameObject(0, 192, 65, 223),
+GameObject(524, 165, 275, 140),
+GameObject(191, 0, 294, 57),
+GameObject(688, 344, 87, 224)]
