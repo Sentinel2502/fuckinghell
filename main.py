718d1d60a2715a8004db0ad96a336c051fd761b0
Sentinel <wildpython@yandex.ru>
2019-11-28 01:44:56 +0300

    Реализован переход между локациями, отдельное хранение информации о локациях, обновлены задники

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
+"Главный файл для запуска программы. Нужен для отладки функций движка"
+
 import pygame, sys
-import GameObject
-from GameObject import *
-import intersectionObjects
-from intersectionObjects import *
+from gameObject import *
+from locations import *
+from information import *
 
-# проверяет, находимся ли мы в границах окна
-def checkIfOutBoundary(character, x, y):
-    if x + characterSize[0] > 800:
-        x = 800 - characterSize[0]
-
-    if y + characterSize[1] > 600:
-        y = 600 - characterSize[1]
-
-    if y <= 0:
-        y = 0
-    if x <= 0:
-        x = 0
-
-    return x, y
 
 #Initialize pygame
 pygame.init()
 pygame.mixer.init()
-windowSize = (800, 600)
+windowSize = (1024, 768)
 screen = pygame.display.set_mode(windowSize)
 pygame.mouse.set_visible(0)
 
 #Resources
-character = pygame.image.load("images/charactes/ggg.png")
-background = pygame.image.load("images/locations/bedroom.png")
-
+character = pygame.image.load("images/characters/char.png")
+background = locationObjectsList["bedroom"]
 characterSize = character.get_size()
 
-x, y = 200, 100
+#Настраиваю штуки (потом всю информацию о настройках для определенных локаций перенести в отдельный файл)
+"""background.setGameField(GameObject(0, 374, 1024, 374))
+background.setIntersectionObjectsList(bedroomIntersectionObjectsList)
+background.setExitObjectsList(bedroomExitObjectsList)"""
+
+x, y = 710, 575
+isIngameField = True
 
 # создаем "хитбокс" для персонажа
 logo = GameObject(x, y, characterSize[0], characterSize[1])
@@ -48,8 +40,8 @@ while True:
     logo.setPosition(x, y)
 
     #проверяю объекты на предмет столкновения с персонажем
-    for i in range(len(bedroom)):
-        if logo.intersects(bedroom[i]):
+    for i in range(len(background.intersectionObjectsList)):
+        if logo.intersects(background.intersectionObjectsList[i]):
             if pressed[pygame.K_RIGHT]:
                 x -= 5
             if pressed[pygame.K_LEFT]:
@@ -59,6 +51,24 @@ while True:
             if pressed[pygame.K_DOWN]:
                 y -= 5
 
+    #проверяю персонажа на предмет столкновения с "выходами"ъ
+    for i in range(len(background.exitObjectsList)):
+        if not logo.intersects(background.exitObjectsList[i][0]):
+            pass
+        else:
+            background = locationObjectsList[background.exitObjectsList[i][1]]
+            x, y = background.exit_x, background.exit_y
+
+    #проверяем нахождение персонажа в границах игровой зоны
+    if not logo.intersects(background.gameFieldObject):
+        if pressed[pygame.K_RIGHT]:
+            x -= 5
+        if pressed[pygame.K_LEFT]:
+            x += 5
+        if pressed[pygame.K_UP]:
+            y += 5
+        if pressed[pygame.K_DOWN]:
+            y -= 5
 
     #реализую движение персонажа по экрану
     pressed = pygame.key.get_pressed()
@@ -71,12 +81,10 @@ while True:
     if pressed[pygame.K_DOWN]:
         y += 5
 
-    x, y = checkIfOutBoundary(character, x, y)
 
-    screen.fill((0, 0, 0))
-    screen.blit(background, (0, 0))
+
+    screen.blit(background.background, (0, 0))
     screen.blit(character, (x, y))
-
 
 
     pygame.display.update()
